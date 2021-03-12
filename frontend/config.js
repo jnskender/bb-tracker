e3283eec288095e2dac12e3e08847a0f44b5a595
jnskender <jnskender@gmail.com>
2021-03-11 20:23:45 -0600

    add prodEndpoint to config

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:1337/graphql`;
-export const prodEndpoint = `http://localhost:1337/graphql`
+export const prodEndpoint = `https://bb-tracker.herokuapp.com/graphql`
